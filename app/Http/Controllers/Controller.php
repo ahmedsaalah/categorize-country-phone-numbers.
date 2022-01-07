e057731a8f2b3614db772dd998690d36779c5826
ahmed salah <asalah@silicon-mind.com>
2022-01-07 10:56:42 +0200

    Add base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct() {
+    
+    }
+    public function getSuccessResponse($data = "")
+    {
+        return response()->json($data);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+     'prefix' => 'customers',
+     'namespace' => 'App\Http\Controllers',
+], function ($router) {
+    Route::get('/', 'CustomerController@index');
+   
 });
