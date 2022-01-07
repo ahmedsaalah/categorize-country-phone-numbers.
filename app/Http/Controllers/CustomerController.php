5ef6752ef88979c97040713bc2f0442ba8709c67
ahmed salah <asalah@silicon-mind.com>
2022-01-07 19:50:38 +0200

    check on data before send to service

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -1,10 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
+use App\Http\Requests\GetCustomersRequest;
 use App\Services\CustomerService;
-
-
+use App\Constants\Paginate;
 
 class CustomerController extends Controller
 {
@@ -18,8 +17,13 @@ class CustomerController extends Controller
         $this->customerService = $customerService;
     }
 
-    public function index(Request $request) {
-        $countries = $this->customerService->getCustomers();
+    public function index(GetCustomersRequest $request) {
+        $country =($request->country)?$request->country:null;
+        $state =($request->state)?$request->state:null;
+        $page =($request->page)?$request->page:Paginate::PAGE;
+        $per_page =($request->per_page)?$request->per_page:Paginate::PER_PAGE;
+
+        $countries = $this->customerService->getCustomers($country, $state, $page, $per_page);
         return $this->getSuccessResponse($countries);
     }
 
